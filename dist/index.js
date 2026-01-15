00d5586fa85601e877753c51e9849efdb292b2fd
Velvet <velvet.pinkish@gmail.com>
2026-01-15 16:04:06 +0100

    Make non-list data visible on the website

--- dist/index.js
+++ dist/index.js
@@ -4,21 +4,76 @@ const trackSelection = [
         id: 1,
         title: "I Really Want to Stay At Your House",
         artist: "Rosa Walton",
+        coverUrl: "https://picsum.photos/300/200",
         category: {
             typeOfMedia: "Anime",
             genre: "EDM",
         },
         difficulty: ["Easy", "Normal"],
-        tempoInBPM: 128,
+        BPM: 128,
+        isPaused: false,
+    },
+    {
+        id: 2,
+        title: "",
+        artist: "",
+        coverUrl: "https://picsum.photos/300/200",
+        category: {
+            typeOfMedia: "",
+            genre: "",
+        },
+        difficulty: ["Easy"],
+        BPM: 0,
+        isPaused: false,
+    },
+    {
+        id: 3,
+        title: "",
+        artist: "",
+        coverUrl: "https://picsum.photos/300/200",
+        category: {
+            typeOfMedia: "",
+            genre: "",
+        },
+        difficulty: ["Easy"],
+        BPM: 0,
+        isPaused: false,
+    },
+    {
+        id: 4,
+        title: "",
+        artist: "",
+        coverUrl: "https://picsum.photos/300/200",
+        category: {
+            typeOfMedia: "",
+            genre: "",
+        },
+        difficulty: ["Easy"],
+        BPM: 0,
         isPaused: false,
     }
 ];
 const trackTitleElement = document.getElementById("track-title");
 const trackArtistElement = document.getElementById("track-artist");
 const coverImageElement = document.getElementById("track-cover");
+// const trackCategoryElement = document.getElementById("track-category");
+const trackBPMElement = document.getElementById("track-tempo");
 const currentTrack = trackSelection[0];
+if (!currentTrack)
+    throw new Error("No tracks in trackSelection");
 if (trackTitleElement) {
     trackTitleElement.textContent = currentTrack.title;
+}
+if (trackArtistElement) {
+    trackArtistElement.textContent = currentTrack.artist;
+}
+if (coverImageElement) {
+    if (currentTrack.coverUrl) {
+        coverImageElement.src = currentTrack.coverUrl;
+    }
+}
+if (trackBPMElement) {
+    trackBPMElement.textContent = currentTrack.BPM.toString();
 }
 export {};
 //# sourceMappingURL=index.js.map
