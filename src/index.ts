2b2ac118f46ec68511b650f9513f63683c318bcf
Velvet <velvet.pinkish@gmail.com>
2026-01-19 19:19:15 +0100

    Add form and restructure TS for deconstruction

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ interface Track {
     id: number;
     title: string;
     artist: string;
+    durationInSeconds: number;
     category: Category;
     difficulty: string[];
     BPM: number;
@@ -23,6 +24,7 @@ const trackSelection: Track[] = [
         id: 1,
         title: "I Really Want to Stay At Your House",
         artist: "Rosa Walton",
+        durationInSeconds: 260,
         coverUrl: "https://picsum.photos/300/200",
         category: {
             typeOfMedia: "Anime",
@@ -37,6 +39,7 @@ const trackSelection: Track[] = [
         id: 2,
         title: "I Really Want to Stay At Your House",
         artist: "Rosa Walton",
+        durationInSeconds: 260,
         coverUrl: "https://picsum.photos/300/200",
         category: {
             typeOfMedia: "Anime",
@@ -50,6 +53,7 @@ const trackSelection: Track[] = [
         id: 3,
         title: "I Really Want to Stay At Your House",
         artist: "Rosa Walton",
+        durationInSeconds: 260,
         coverUrl: "https://picsum.photos/300/200",
         category: {
             typeOfMedia: "Anime",
@@ -64,6 +68,7 @@ const trackSelection: Track[] = [
         id: 4,
         title: "I Really Want to Stay At Your House",
         artist: "Rosa Walton",
+        durationInSeconds: 260,
         coverUrl: "https://picsum.photos/300/200",
         category: {
             typeOfMedia: "Anime",
@@ -90,83 +95,116 @@ const trackBPMElement = document.getElementById("track-tempo");
 //Lists
 const trackContainer = document.querySelector("#track-container");
 
-//Logic
+//Dialogs
 
-trackSelection.forEach((track) => {
-    const card = document.createElement("article");
-    card.classList.add("main-body__card");
+const dialog = document.querySelector("#add-track-dialog") as HTMLDialogElement;
+const openBtn = document.querySelector('#open-modal-btn') as HTMLButtonElement;
+const closeBtn = document.querySelector("#close-modal-btn") as HTMLButtonElement;
 
-    const title = document.createElement("h2");
-    card.classList.add("main-body__track-title");
-    title.textContent = track.title;
+const addForm = document.querySelector("#add-track-form") as HTMLFormElement;
+const titleInput = document.querySelector("#title-input") as HTMLInputElement;
+const artistInput = document.querySelector("#artist-input") as HTMLInputElement;
+const durationInput = document.querySelector("#duration-input") as HTMLInputElement;
 
-    const artist = document.createElement("span");
-    card.classList.add("main-body__track-artist")
-    artist.textContent = track.artist;
-
-    const imageWrapper = document.createElement("div");
-    imageWrapper.classList.add("main-body__image-wrapper");
-
-    if(track.coverUrl) {
-        const image = document.createElement("img");
-        image.classList.add("main-body__image");
-        image.src = track.coverUrl;
-        image.alt = `Lorem Ipsum`;
-
-        imageWrapper.appendChild(image);
-    }
-
-    const difficultyWrapper = document.createElement("div");
-    difficultyWrapper.classList.add("main-body__track-difficulty");
-
-    for(const difficulty of track.difficulty) {
-        difficultyWrapper.append(difficulty);
-    }
-
-    const bpm = document.createElement("span");
-    card.classList.add("main-body__track-tempo");
-    bpm.textContent = track.BPM.toString();
-
-    const typeOfMedia = document.createElement("span");
-    card.classList.add("main-body__track-category--media")
-    typeOfMedia.textContent = track.category.typeOfMedia;
-
-    const genre = document.createElement("span");
-    card.classList.add("main-body__track-category--genre");
-    genre.textContent = track.category.genre;
-
-    card.append(
-        imageWrapper, 
-        title, 
-        artist, 
-        difficultyWrapper,
-        bpm,
-        typeOfMedia,
-        genre
-    );
+function renderTracks() {
 
     if(trackContainer) {
-        card.addEventListener("mouseenter", () => {
-            const currentActive = document.querySelector(".main-body__card.active");
-
-            //Enable CSS visuals if card is active
-            if(currentActive) {
-                currentActive.classList.remove("active");
-            }
-
-            card.classList.add("active");
-            //TODO: Add music on mouseenter
-            previewTrack(track);
-        })
-
-        card.addEventListener("mouseleave", () => {
-           card.classList.remove("active");
-           //TODO: Add removal of music
-        })
-
-        trackContainer.appendChild(card);
+        trackContainer.replaceChildren();
     }
-})
+
+    const heading = document.createElement("h1");
+    heading.classList.add("main-body__heading");
+    heading.textContent = "Select your track!";
+    trackContainer?.appendChild(heading);
+
+    trackSelection.forEach((track) => {
+
+        //destructuring
+        const { title, artist, durationInSeconds } = track;
+
+        const card = document.createElement("article");
+        card.classList.add("main-body__card");
+
+        const titleElement = document.createElement("h2");
+        card.classList.add("main-body__track-title");
+        titleElement.textContent = title;
+
+        const artistElement = document.createElement("span");
+        card.classList.add("main-body__track-artist")
+        artistElement.textContent = artist;
+
+        const durationElement = document.createElement("span");
+        card.classList.add("main-body__track-duration");
+        durationElement.textContent = durationInSeconds.toString();
+
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("main-body__image-wrapper");
+
+        if(track.coverUrl) {
+            const image = document.createElement("img");
+            image.classList.add("main-body__image");
+            image.src = track.coverUrl;
+            image.alt = `Lorem Ipsum`;
+
+            imageWrapper.appendChild(image);
+        }
+
+        const difficultyWrapper = document.createElement("div");
+        difficultyWrapper.classList.add("main-body__track-difficulty");
+
+        for(const difficulty of track.difficulty) {
+            difficultyWrapper.append(difficulty);
+        }
+
+        const bpm = document.createElement("span");
+        card.classList.add("main-body__track-tempo");
+        bpm.textContent = track.BPM.toString();
+
+        const typeOfMedia = document.createElement("span");
+        card.classList.add("main-body__track-category--media")
+        typeOfMedia.textContent = track.category.typeOfMedia;
+
+        const genre = document.createElement("span");
+        card.classList.add("main-body__track-category--genre");
+        genre.textContent = track.category.genre;
+
+        card.append(
+            imageWrapper, 
+            title, 
+            artist, 
+            difficultyWrapper,
+            bpm,
+            typeOfMedia,
+            genre
+        );
+
+        if(trackContainer) {
+            card.addEventListener("mouseenter", () => {
+                const currentActive = document.querySelector(".main-body__card.active");
+
+                //Enable CSS visuals if card is active
+                if(currentActive) {
+                    currentActive.classList.remove("active");
+                }
+
+                card.classList.add("active");
+                //TODO: Add music on mouseenter
+                previewTrack(track);
+            })
+
+            card.addEventListener("mouseleave", () => {
+            card.classList.remove("active");
+            //TODO: Add removal of music
+            })
+
+            trackContainer.appendChild(card);
+        }
+    })
+}
+
+// --- Logic ---
+
+renderTracks();
 
 // --- Functions ---
 
@@ -201,3 +239,13 @@ function previewTrack(track: Track) {
 
 // const currentTrack = trackSelection[0];
 // if (!currentTrack) throw new Error("No tracks in trackSelection");
+
+// --- Modals ---
+
+openBtn.addEventListener("click", () => {
+    dialog.showModal()
+});
+
+closeBtn.addEventListener("click", () => {
+    dialog.close()
+});
